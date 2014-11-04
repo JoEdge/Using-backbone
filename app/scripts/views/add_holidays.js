f1567827570f2fd3b44d03b375d5d240527c1bc1
Joanna Edgell <joedgell2013@gmail.com>
2014-11-04 16:27:14 -0500

    trying to add_holidays

---
+++ app/scripts/views/add_holidays.js
@@ -0,0 +1,55 @@
+
+(function () {
+
+App.Views.AddHolidays = Backbone.View.extend ({
+
+  el:'#holidayAdder',
+//always delegated events
+  events: {
+    "submit #holidayForm" : "addNewHoliday"
+  },
+
+  initialize: function() {
+    this.render();
+  },
+
+  render:function(){
+    var form_html = $('#addHoliday').html();
+    this.$el.html(form_html);
+  },
+
+  addNewHoliday: function(h) {
+    h.preventDefault();
+    alert('I was submitted')
+
+  //grab info from input
+  var holiday_name = $("#name").val();
+  var holiday_date = $("#date").val();
+  var holiday_character = $("#character").val();
+  var holiday_food = $("#food").val();
+  var holiday_plant = $("#plant").val();
+
+
+  var smile = new App.Models.Holiday ({
+    name: holiday_name,
+    date: holiday_date,
+    character: holiday_character,
+    food: holiday_food,
+    plant: holiday_plant
+  });
+
+
+    //access our collection and add new instances to collection
+    App.all_holidays.add(smile);
+
+    //save our holiday
+    smile.save();
+
+    console.log(smile);
+
+    //clear my form
+    $(this)[0].reset();
+}
+});
+
+}());
