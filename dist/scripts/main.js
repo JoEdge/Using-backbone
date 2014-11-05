50e426944f01c8d00f91eac7d01b3588038301cf
Joanna Edgell <joedgell2013@gmail.com>
2014-11-05 14:36:25 -0500

    basic styling and working

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -138,6 +138,73 @@ App.Views.ListHolidays = Backbone.View.extend ({
 
 }());
 
+var toy = {};
+
+(function () {
+
+  App.Views.SingleHoliday = Backbone.View.extend({
+
+    tagName: 'ul',
+    className: 'coffeeSingle',
+
+    events: {
+      'click #commentButton' : 'updateHoliday',
+      'click #delete' : 'deleteHoliday'
+    },
+
+    template: _.template($('#singleHoliday').html()),
+
+    initialize: function (options) {
+      this.options = options;
+      this.render();
+
+      $('#holidayAdder').empty();
+
+      // Get our Element On Our Page
+      $('#happy_holidays').html(this.$el);
+    },
+
+    render: function () {
+
+      this.$el.empty();
+
+      this.$el.html(this.template(this.options.holiday.toJSON()));
+
+    },
+
+    updateHoliday: function (e) {
+      e.preventDefault();
+
+      this.$el.empty();
+
+      // Update our Model Instance
+      this.options.holiday.set({
+        comments: $('#holiday_comments').val()
+      });
+
+      // Save Instance
+      this.options.holiday.save();
+
+
+      // Go back to our home page
+      App.router.navigate('', {trigger: true});
+
+    },
+
+    deleteHoliday: function (e) {
+      e.preventDefault();
+
+      // Remove Coffee
+      this.options.holiday.destroy();
+
+      // Go home ET
+      App.router.navigate('', {trigger: true});
+
+    }
+
+  });
+
+}());
 
 (function () {
 
