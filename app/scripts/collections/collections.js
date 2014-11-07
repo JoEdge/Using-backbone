63bec5c116272ebe733d65303b3fddc71abe8cda
Joanna Edgell <joedgell2013@gmail.com>
2014-11-07 10:01:11 -0500

    sort by event name

--- app/scripts/collections/collections.js
+++ app/scripts/collections/collections.js
@@ -3,7 +3,10 @@ var my_server = "http://tiy-atl-fe-server.herokuapp.com/collections/joanna3";
 
 App.Collections.HolidaysCollection = Backbone.Collection.extend ({
   model: App.Models.Holiday,
-  url: my_server
+    comparator: function (model) {
+    return (model.get('event_shop'));
+    },
+  url: my_server,
 
 });
 
